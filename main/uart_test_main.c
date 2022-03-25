8404c28a55a41f23358fc41a8dccf31ae6e59947
qitas <qitas@qitas.cn>
2022-03-25 15:57:37 +0800

    fix

--- main/uart_test_main.c
+++ main/uart_test_main.c
@@ -21,15 +21,17 @@
 #define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
 #define TEST_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TEST_FLASH_SIZE)
 
+#define BUF_SIZE        (100)
+#define TEST_F_START    (0x50000)
 // static const char *TAG = "TEST";
 static uint16_t sector_max = 1;
 static uint16_t sector_num = 1;
 static int32_t test_sector = 0;
 static uint32_t test_round = 0;
 static uint32_t flash_tested_round = 0;
+static uint32_t sector_offset = 0;
+static uint32_t addr_offset = TEST_F_START;
 
-#define BUF_SIZE        (1024)
-#define TEST_F_START    (0x50000)
 #define BLOCK_SIZE      SPI_FLASH_SEC_SIZE
 static char test_data[BLOCK_SIZE];
 static char test_buff[BLOCK_SIZE];
@@ -43,13 +45,12 @@ static IRAM_ATTR void test_task(void *arg)
     // size_t size_flash_chip = 0x200000;
     sector_max = (size_flash_chip-TEST_F_START)/BLOCK_SIZE;
     // sector_max = (size_flash_chip)/BLOCK_SIZE;
-    char uart_info[80];
+    // char uart_info[BUF_SIZE];
+    char *uart_info = (char *) malloc(BUF_SIZE);
     for (int i = 0; i < BLOCK_SIZE; i++) {
         test_data[i] = i%250 + 1;
     }
-    uint32_t sector_offset = test_sector*BLOCK_SIZE;
-    uint32_t addr_offset =  TEST_F_START + sector_offset;
-    memset(uart_info, 0x0, sizeof(uart_info));
+    memset(uart_info, 0x0, strlen(uart_info));
     sprintf(uart_info, "TEST FLASH [%d MB]",size_flash_chip / (1024 * 1024));
     uart_write_bytes(ECHO_UART_PORT_NUM, uart_info, strlen(uart_info));
     while (1) {
@@ -58,8 +59,8 @@ static IRAM_ATTR void test_task(void *arg)
         // xTaskNotifyWait(0, 0xFFFFFFFF, &notify_value, portMAX_DELAY);
         if(test_round && flash_tested_round>=test_round) {
             sector_num --;
-            memset(uart_info, 0x0, sizeof(uart_info));
-            sprintf(uart_info, "[%d][0x%x]sector test %d times:pass",test_sector,addr_offset,test_round);
+            memset(uart_info, 0x0, strlen(uart_info));
+            sprintf(uart_info, "[%d][0x%x] test %d c:pass",test_sector,addr_offset,test_round);
             // ESP_LOGI(TAG, "%s",uart_info);
             uart_write_bytes(ECHO_UART_PORT_NUM, uart_info, strlen(uart_info));
             if(sector_num==0) {
@@ -90,7 +91,7 @@ static IRAM_ATTR void test_task(void *arg)
             for (int i = 0; i < sizeof(test_buff); i++) {
                 if(test_buff[i] != 0xFF){
                     // ESP_LOGI(TAG, "erase sector %d data err[%d/%d]: %d!=0xFF",test_sector, flash_tested_round,test_round , test_buff[i]);
-                    memset(uart_info, 0x0, sizeof(uart_info));
+                    memset(uart_info, 0x0, strlen(uart_info));
                     sprintf(uart_info, "test sector %d:erase err[%d/%d]",test_sector, flash_tested_round,test_round);
                     uart_write_bytes(ECHO_UART_PORT_NUM, uart_info, strlen(uart_info));
                     test_round = 0;
@@ -108,7 +109,7 @@ static IRAM_ATTR void test_task(void *arg)
             for (int i = 0; i < sizeof(test_buff); i++) {
                 if(test_buff[i] != test_data[i]){
                     // ESP_LOGI(TAG, "write sector %d data err[%d/%d]: %d!=%d",test_sector, flash_tested_round,test_round , test_buff[i] , test_data[i]);
-                    memset(uart_info, 0x0, sizeof(uart_info));
+                    memset(uart_info, 0x0, strlen(uart_info));
                     sprintf(uart_info, "test sector %d:write err[%d/%d]",test_sector, flash_tested_round,test_round);
                     uart_write_bytes(ECHO_UART_PORT_NUM, uart_info, strlen(uart_info));
                     test_round = 0;
@@ -125,7 +126,6 @@ static IRAM_ATTR void test_task(void *arg)
 
 static IRAM_ATTR void uart_task(void *arg)
 {
-
     uart_config_t uart_config = {
         .baud_rate = ECHO_UART_BAUD_RATE,
         .data_bits = UART_DATA_8_BITS,
@@ -145,25 +145,25 @@ static IRAM_ATTR void uart_task(void *arg)
 
     // Configure a temporary buffer for the incoming data
     uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-    char uart_info[80];
-
+    char *uart_info = (char *) malloc(BUF_SIZE);
+    // char uart_info[BUF_SIZE];
     while (1) {
         // Read data from the UART
         int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_PERIOD_MS);
-        if (len>12) {
+        if (len>11) {
             data[len] = '\0';
-            memset(uart_info, 0x0, sizeof(uart_info));
+            memset(uart_info, 0x0, strlen(uart_info));
             // ESP_LOGI(TAG, "Recv Str[%d]: %s",len , (char *) data);
             sprintf(uart_info, "RecvStr[%d]:%s",len , (char *) data);
             // ESP_LOGI(TAG,"%s",uart_info);
             uart_write_bytes(ECHO_UART_PORT_NUM, uart_info, strlen(uart_info));
-            if (memcmp((char *) data, "Flash sector ", 13)==0 || memcmp((char *) data, "flash sector ", 13)==0) {
+            if (memcmp((char *) data, "test sector ", 12)==0) {
                 // ESP_LOGI(TAG, "Recv[%d][%d][%d]: %s",len , data[13] , data[15] , (char *) data);
                 flag = 0;
                 test_round = 0;
                 test_sector = 0;
                 flash_tested_round = 0;
-                for (int i = 13; i < len; i++) {
+                for (int i = 12; i < len; i++) {
                     if(flag<=1 && data[i]>='0' && data[i]<='9'){
                         test_sector *= 10;
                         test_sector += data[i]-'0';
@@ -178,6 +178,14 @@ static IRAM_ATTR void uart_task(void *arg)
                         flag = 2;
                     }
                     else if((flag==1 || flag==2) && data[i]==' '){
+                        if(flag==1){
+                            sector_offset = test_sector*BLOCK_SIZE;
+                            addr_offset = TEST_F_START + sector_offset;
+                        }
+                        else {
+                            sector_offset = 0;
+                            addr_offset = TEST_F_START ;
+                        }
                         flag = 3;
                     }
                     else if(flag>=3 && data[i]>='0' && data[i]<='9'){
@@ -194,9 +202,9 @@ static IRAM_ATTR void uart_task(void *arg)
                 }
                 if(test_round){
                     if(sector_num==1){
-                        sprintf(uart_info, "ready test sector %d:%d ",test_sector,test_round);
+                        sprintf(uart_info, "ready test sector [%d][0x%x]:%d cycles",test_sector,addr_offset,test_round);
                     }
-                    else sprintf(uart_info, "ready test all sector:%d ",test_round);
+                    else sprintf(uart_info, "ready test all sector:%d cycles",test_round);
                     // ESP_LOGI(TAG,"%s",uart_info);
                     uart_write_bytes(ECHO_UART_PORT_NUM, uart_info, strlen(uart_info));
                 }
