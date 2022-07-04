751728f98eb12b1b67c27b2cf0c34b1bd870d94b
qitas <qitas@qitas.cn>
2022-07-04 18:28:36 +0800

    update

---
+++ components/hello_world_lib.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+#include "hello_world_lib.h"
+/*******************************************************************************
+**函数信息 ：
+**功能描述 ：
+**输入参数 ：
+**输出参数 ：
+********************************************************************************/
+void hello_world_print(void)
+{
+    printf("Hello world with lib!\n");
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+            CONFIG_IDF_TARGET,
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+}
